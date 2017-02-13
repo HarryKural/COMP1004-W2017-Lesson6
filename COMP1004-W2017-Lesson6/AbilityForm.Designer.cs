5112e0bcee451e560a94a6127397fab3a23592c6
HarryKural <harrykural333@gmail.com>
2017-02-13 13:42:18 -0500

    Added Ability Form

---
+++ COMP1004-W2017-Lesson6/AbilityForm.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace COMP1004_W2017_Lesson6
+{
+    partial class AbilityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FightingLabel = new System.Windows.Forms.Label();
+            this.FightingTextBox = new System.Windows.Forms.TextBox();
+            this.AgilityTextBox = new System.Windows.Forms.TextBox();
+            this.AgilityLabel = new System.Windows.Forms.Label();
+            this.EnduranceTextBox = new System.Windows.Forms.TextBox();
+            this.EnduranceLabel = new System.Windows.Forms.Label();
+            this.StrengthTextBox = new System.Windows.Forms.TextBox();
+            this.StrengthLabel = new System.Windows.Forms.Label();
+            this.PsycheTextBox = new System.Windows.Forms.TextBox();
+            this.PsycheLabel = new System.Windows.Forms.Label();
+            this.IntuitionTextBox = new System.Windows.Forms.TextBox();
+            this.IntuitionLabel = new System.Windows.Forms.Label();
+            this.ReasonTextBox = new System.Windows.Forms.TextBox();
+            this.ReasonLabel = new System.Windows.Forms.Label();
+            this.KarmaTextBox = new System.Windows.Forms.TextBox();
+            this.KarmaLabel = new System.Windows.Forms.Label();
+            this.HealthTextBox = new System.Windows.Forms.TextBox();
+            this.HealthLabel = new System.Windows.Forms.Label();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.PhysicalAbilitiesGroupBox = new System.Windows.Forms.GroupBox();
+            this.MentalAbilitiesGroupBox = new System.Windows.Forms.GroupBox();
+            this.PhysicalAbilitiesGroupBox.SuspendLayout();
+            this.MentalAbilitiesGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FightingLabel
+            // 
+            this.FightingLabel.AutoSize = true;
+            this.FightingLabel.Location = new System.Drawing.Point(14, 25);
+            this.FightingLabel.Name = "FightingLabel";
+            this.FightingLabel.Size = new System.Drawing.Size(74, 20);
+            this.FightingLabel.TabIndex = 0;
+            this.FightingLabel.Text = "Fighting";
+            // 
+            // FightingTextBox
+            // 
+            this.FightingTextBox.Location = new System.Drawing.Point(14, 52);
+            this.FightingTextBox.Name = "FightingTextBox";
+            this.FightingTextBox.Size = new System.Drawing.Size(100, 26);
+            this.FightingTextBox.TabIndex = 1;
+            // 
+            // AgilityTextBox
+            // 
+            this.AgilityTextBox.Location = new System.Drawing.Point(14, 112);
+            this.AgilityTextBox.Name = "AgilityTextBox";
+            this.AgilityTextBox.Size = new System.Drawing.Size(100, 26);
+            this.AgilityTextBox.TabIndex = 3;
+            // 
+            // AgilityLabel
+            // 
+            this.AgilityLabel.AutoSize = true;
+            this.AgilityLabel.Location = new System.Drawing.Point(14, 85);
+            this.AgilityLabel.Name = "AgilityLabel";
+            this.AgilityLabel.Size = new System.Drawing.Size(57, 20);
+            this.AgilityLabel.TabIndex = 2;
+            this.AgilityLabel.Text = "Agility";
+            // 
+            // EnduranceTextBox
+            // 
+            this.EnduranceTextBox.Location = new System.Drawing.Point(14, 233);
+            this.EnduranceTextBox.Name = "EnduranceTextBox";
+            this.EnduranceTextBox.Size = new System.Drawing.Size(100, 26);
+            this.EnduranceTextBox.TabIndex = 7;
+            // 
+            // EnduranceLabel
+            // 
+            this.EnduranceLabel.AutoSize = true;
+            this.EnduranceLabel.Location = new System.Drawing.Point(14, 206);
+            this.EnduranceLabel.Name = "EnduranceLabel";
+            this.EnduranceLabel.Size = new System.Drawing.Size(96, 20);
+            this.EnduranceLabel.TabIndex = 6;
+            this.EnduranceLabel.Text = "Endurance";
+            // 
+            // StrengthTextBox
+            // 
+            this.StrengthTextBox.Location = new System.Drawing.Point(14, 173);
+            this.StrengthTextBox.Name = "StrengthTextBox";
+            this.StrengthTextBox.Size = new System.Drawing.Size(100, 26);
+            this.StrengthTextBox.TabIndex = 5;
+            // 
+            // StrengthLabel
+            // 
+            this.StrengthLabel.AutoSize = true;
+            this.StrengthLabel.Location = new System.Drawing.Point(14, 146);
+            this.StrengthLabel.Name = "StrengthLabel";
+            this.StrengthLabel.Size = new System.Drawing.Size(79, 20);
+            this.StrengthLabel.TabIndex = 4;
+            this.StrengthLabel.Text = "Strength";
+            // 
+            // PsycheTextBox
+            // 
+            this.PsycheTextBox.Location = new System.Drawing.Point(12, 173);
+            this.PsycheTextBox.Name = "PsycheTextBox";
+            this.PsycheTextBox.Size = new System.Drawing.Size(100, 26);
+            this.PsycheTextBox.TabIndex = 13;
+            // 
+            // PsycheLabel
+            // 
+            this.PsycheLabel.AutoSize = true;
+            this.PsycheLabel.Location = new System.Drawing.Point(12, 146);
+            this.PsycheLabel.Name = "PsycheLabel";
+            this.PsycheLabel.Size = new System.Drawing.Size(66, 20);
+            this.PsycheLabel.TabIndex = 12;
+            this.PsycheLabel.Text = "Psyche";
+            // 
+            // IntuitionTextBox
+            // 
+            this.IntuitionTextBox.Location = new System.Drawing.Point(12, 113);
+            this.IntuitionTextBox.Name = "IntuitionTextBox";
+            this.IntuitionTextBox.Size = new System.Drawing.Size(100, 26);
+            this.IntuitionTextBox.TabIndex = 11;
+            // 
+            // IntuitionLabel
+            // 
+            this.IntuitionLabel.AutoSize = true;
+            this.IntuitionLabel.Location = new System.Drawing.Point(12, 86);
+            this.IntuitionLabel.Name = "IntuitionLabel";
+            this.IntuitionLabel.Size = new System.Drawing.Size(75, 20);
+            this.IntuitionLabel.TabIndex = 10;
+            this.IntuitionLabel.Text = "Intuition";
+            // 
+            // ReasonTextBox
+            // 
+            this.ReasonTextBox.Location = new System.Drawing.Point(12, 52);
+            this.ReasonTextBox.Name = "ReasonTextBox";
+            this.ReasonTextBox.Size = new System.Drawing.Size(100, 26);
+            this.ReasonTextBox.TabIndex = 9;
+            // 
+            // ReasonLabel
+            // 
+            this.ReasonLabel.AutoSize = true;
+            this.ReasonLabel.Location = new System.Drawing.Point(12, 25);
+            this.ReasonLabel.Name = "ReasonLabel";
+            this.ReasonLabel.Size = new System.Drawing.Size(71, 20);
+            this.ReasonLabel.TabIndex = 8;
+            this.ReasonLabel.Text = "Reason";
+            // 
+            // KarmaTextBox
+            // 
+            this.KarmaTextBox.Location = new System.Drawing.Point(145, 51);
+            this.KarmaTextBox.Name = "KarmaTextBox";
+            this.KarmaTextBox.Size = new System.Drawing.Size(100, 26);
+            this.KarmaTextBox.TabIndex = 17;
+            // 
+            // KarmaLabel
+            // 
+            this.KarmaLabel.AutoSize = true;
+            this.KarmaLabel.Location = new System.Drawing.Point(145, 24);
+            this.KarmaLabel.Name = "KarmaLabel";
+            this.KarmaLabel.Size = new System.Drawing.Size(60, 20);
+            this.KarmaLabel.TabIndex = 16;
+            this.KarmaLabel.Text = "Karma";
+            // 
+            // HealthTextBox
+            // 
+            this.HealthTextBox.Location = new System.Drawing.Point(138, 52);
+            this.HealthTextBox.Name = "HealthTextBox";
+            this.HealthTextBox.Size = new System.Drawing.Size(100, 26);
+            this.HealthTextBox.TabIndex = 15;
+            // 
+            // HealthLabel
+            // 
+            this.HealthLabel.AutoSize = true;
+            this.HealthLabel.Location = new System.Drawing.Point(138, 25);
+            this.HealthLabel.Name = "HealthLabel";
+            this.HealthLabel.Size = new System.Drawing.Size(62, 20);
+            this.HealthLabel.TabIndex = 14;
+            this.HealthLabel.Text = "Health";
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(12, 295);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(99, 35);
+            this.GenerateButton.TabIndex = 18;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(440, 295);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(99, 35);
+            this.NextButton.TabIndex = 19;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // PhysicalAbilitiesGroupBox
+            // 
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.StrengthTextBox);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.FightingLabel);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.FightingTextBox);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.AgilityLabel);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.AgilityTextBox);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.HealthTextBox);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.StrengthLabel);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.HealthLabel);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.EnduranceLabel);
+            this.PhysicalAbilitiesGroupBox.Controls.Add(this.EnduranceTextBox);
+            this.PhysicalAbilitiesGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhysicalAbilitiesGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.PhysicalAbilitiesGroupBox.Name = "PhysicalAbilitiesGroupBox";
+            this.PhysicalAbilitiesGroupBox.Size = new System.Drawing.Size(254, 267);
+            this.PhysicalAbilitiesGroupBox.TabIndex = 20;
+            this.PhysicalAbilitiesGroupBox.TabStop = false;
+            this.PhysicalAbilitiesGroupBox.Text = "Physical Abilities";
+            // 
+            // MentalAbilitiesGroupBox
+            // 
+            this.MentalAbilitiesGroupBox.Controls.Add(this.ReasonLabel);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.ReasonTextBox);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.IntuitionLabel);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.IntuitionTextBox);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.KarmaTextBox);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.PsycheLabel);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.KarmaLabel);
+            this.MentalAbilitiesGroupBox.Controls.Add(this.PsycheTextBox);
+            this.MentalAbilitiesGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MentalAbilitiesGroupBox.Location = new System.Drawing.Point(282, 12);
+            this.MentalAbilitiesGroupBox.Name = "MentalAbilitiesGroupBox";
+            this.MentalAbilitiesGroupBox.Size = new System.Drawing.Size(257, 267);
+            this.MentalAbilitiesGroupBox.TabIndex = 21;
+            this.MentalAbilitiesGroupBox.TabStop = false;
+            this.MentalAbilitiesGroupBox.Text = "Mental Abilities";
+            // 
+            // AbilityForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(551, 343);
+            this.Controls.Add(this.MentalAbilitiesGroupBox);
+            this.Controls.Add(this.PhysicalAbilitiesGroupBox);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.GenerateButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "AbilityForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AbilityForm";
+            this.PhysicalAbilitiesGroupBox.ResumeLayout(false);
+            this.PhysicalAbilitiesGroupBox.PerformLayout();
+            this.MentalAbilitiesGroupBox.ResumeLayout(false);
+            this.MentalAbilitiesGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FightingLabel;
+        private System.Windows.Forms.TextBox FightingTextBox;
+        private System.Windows.Forms.TextBox AgilityTextBox;
+        private System.Windows.Forms.Label AgilityLabel;
+        private System.Windows.Forms.TextBox EnduranceTextBox;
+        private System.Windows.Forms.Label EnduranceLabel;
+        private System.Windows.Forms.TextBox StrengthTextBox;
+        private System.Windows.Forms.Label StrengthLabel;
+        private System.Windows.Forms.TextBox PsycheTextBox;
+        private System.Windows.Forms.Label PsycheLabel;
+        private System.Windows.Forms.TextBox IntuitionTextBox;
+        private System.Windows.Forms.Label IntuitionLabel;
+        private System.Windows.Forms.TextBox ReasonTextBox;
+        private System.Windows.Forms.Label ReasonLabel;
+        private System.Windows.Forms.TextBox KarmaTextBox;
+        private System.Windows.Forms.Label KarmaLabel;
+        private System.Windows.Forms.TextBox HealthTextBox;
+        private System.Windows.Forms.Label HealthLabel;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.GroupBox PhysicalAbilitiesGroupBox;
+        private System.Windows.Forms.GroupBox MentalAbilitiesGroupBox;
+    }
+}
